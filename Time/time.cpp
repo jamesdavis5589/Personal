41f18cabe5b6bfafce1b1e7a8b8ad0f876209d97
James Davis <james.davis5589@outlook.com>
2020-01-23 10:50:11 -0900

    Added milliseconds, days, months, and years.  Updated update() with corresponding info

--- Time/time.cpp
+++ Time/time.cpp
@@ -2,20 +2,39 @@
 James Davis
 January 23rd
 Time - time.h
+This program will define class Time
 */
 
 #include "time.h"
 
 Time::Time() :
+	_milliseconds(0),
 	_seconds(0),
 	_minutes(0),
-	_hours(0)
+	_hours(0),
+	_days(0),
+	_months(0),
+	_years(0)
 {}
 
 void Time::update()
 {
 	//Will update the time variables, making each appropriate
 
+	//Milliseconds
+	if (_milliseconds <= 1000)
+	{
+		int overflow;
+		overflow = _milliseconds / 1000;
+
+		//Add extra minutes to hours
+		_seconds += overflow;
+
+		//Remove extra minutes
+		_milliseconds += _milliseconds % 1000;
+	}
+
+	//Seconds
 	if (_seconds <= 60)
 	{
 		int overflow;
@@ -28,6 +47,7 @@ void Time::update()
 		_seconds += _seconds % 60;
 	}
 
+	//Minutes
 	if (_minutes <= 60)
 	{
 		int overflow;
@@ -36,7 +56,33 @@ void Time::update()
 		//Add extra minutes to hours
 		_hours += overflow;
 
-		//Remove extra seconds
+		//Remove extra minutes
 		_minutes += _minutes % 60;
+	}
+
+	//Hours
+	if (_hours <= 24)
+	{
+		int overflow;
+		overflow = _hours / 24;
+
+		//Add extra hours to days
+		_days += overflow;
+
+		//Remove extra hours
+		_hours += _hours % 24;
+	}
+
+	//Days
+	if (_days <= 30)
+	{
+		int overflow;
+		overflow = _days / 30;
+
+		//Add extra hours to days
+		_years += overflow;
+
+		//Remove extra hours
+		_days += _days % 30;
 	}
 }
--- Time/time.h
+++ Time/time.h
@@ -2,15 +2,20 @@
 James Davis
 January 23rd
 Time - time.h
+This program will house the class prototype for Time
 */
 
 #ifndef time_h
 #define time_h
 
 class Time {
+	int _milliseconds;
 	int _seconds;
 	int _minutes;
 	int _hours;
+	int _days;
+	int _months;
+	int _years;
 
 public:
 	Time();
